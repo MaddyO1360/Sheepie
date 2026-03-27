8debfa904b9adbc443de2d9ace72cf36a5a98a94
MaddyO1360 <Madeleineosei12@outlook.com>
2026-03-26 22:15:17 -0400

    coins can be collected now!

--- Scripts/coin.gd
+++ Scripts/coin.gd
@@ -17,16 +17,21 @@ func spawn():
 #I think i'm getting an error because of body entered method
 #spawning works but it duplicates for some weird reason
 #the main goal is to just move the coin rather than create a whole new one
+#^^^ it would duplicate bc the sprite2D would move positions but not the whole scene
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		var rng = RandomNumberGenerator.new()
 		# error = area2D node's pos doesn't change with Sprite2D's pos
-		#I could change the node to a collision object or find a way to
-		#delete the area2D or change it's pos alongside the Sprite2D
-		$Sprite2D.global_position.x = rng.randi_range(10, 200)
-		$Sprite2D.global_position.y = rng.randi_range(10, 100)
-		$Sprite2D.set_global_position(Vector2($Sprite2D.global_position.x, 50))
+		#^^^ this is because when you add $Sprite2D. ---, that limits what's being moved
+		#so, just put in the ---- that you want to move the entire scene
+		global_position.x = rng.randi_range(100, 1000)
+		global_position.y = rng.randi_range(100, 675)
+		set_global_position(Vector2(global_position.x, global_position.y))
 		
 		
 		
 	
+
+
+func _on_tile_map_layer_child_entered_tree(node: Node) -> void:
+	pass # Replace with function body.
